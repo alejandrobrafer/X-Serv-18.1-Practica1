db1216e24939d0726d2c069dc4610df15225c3c3
Alejandro Bravo Fernández <alejandrobravofernandez@ubuntu.(none)>
2018-03-07 02:55:20 -0800

    algunas mejoras

--- practica1.py
+++ practica1.py
@@ -9,39 +9,30 @@ import webapp
 from urllib.parse import unquote
 
 PREFIX = 'http://'
-PRACTICE_NAME = "<html><head><title>URL shortener~SARO_2018</title></head></html>"
-
-
+PRACTICE_NAME = "<head><title>URL shortener~SARO_2018</title></head>"
+port = 1234
+machine = "localhost"
 origi_URL_dic = {}
 simpli_URL_dic = {}
 
-def redirector(url):
-	return ("<html><head><title>URL shortener~SARO_2018</title>" +
-	"<META HTTP-EQUIV='REFRESH' CONTENT='5;URL=" + str(url) + "'>" +
+def router(url):
+	return ("<html>" + PRACTICE_NAME + "<head><META HTTP-EQUIV='REFRESH' CONTENT='5;URL=" + str(url) + "'>" +
 	"</head><body>Redirecting in 5 seconds ...</body></html>")
 
 form = """
-<html>
-<head><title>URL shortener~SARO_2018</title></head>
-<body>
-<h1><font color="Blue"><center>Welcome to the URL shortener!</center></font></h1>
-<form action="" method="POST">
-Your original URL here:
-<input type="text" name="URL" value="None"/>
-<br/>
-<input type="submit" value="SHORTEN URL"/>
-</form>
-<br/>
-<h4>We currently have the following shortened URLs:</h4>
-</body>
-</html>
+<h1><font color="Blue"><center><u>URL shortener ~ SARO 2018</u></center></font></h1>
+<br>
+<form action="" method="POST"><input type="text" name="URL" value="" placeholder="Your original URL here"/>
+<input type="submit" value="SHORTEN URL"/><input type="reset" value="Reset"></form>
+<br><br>
+<b><i>List of original and shortened URLs at this time:</i></b>
 """
 
 Codes = {'200': 'OK', '404': 'Not Found', 
 		'302': 'Found', '501': 'Not Implemented'}
 
 def send_response(Code, Body):
-	return (Code + " " + Codes[Code], "<html><body><h1>" + Body + "</h1></body></html>")
+	return (Code + " " + Codes[Code], "<html>" + PRACTICE_NAME + "<body><h1>" + Body + "</h1></body></html>")
 
 class practice1App(webapp.webApp):
 	
@@ -59,7 +50,7 @@ class practice1App(webapp.webApp):
 		"""Process the relevant elements of the request."""
 		# Case: favicon.ico
 		if not parsedRequest:
-			response = send_response('200', 'Go awy!')
+			response = send_response('200', "<center>Go awy!<center>")
 		else:
 			method, resource, petition = parsedRequest
 
@@ -67,18 +58,21 @@ class practice1App(webapp.webApp):
 				response = send_response('200', form + str(origi_URL_dic))
 			
 			elif method == 'GET':
-				num = int(resource.split('/')[1])
+				num = resource.split('/')[1]
+				key = "/" + num
 				
-				if num in origi_URL_dic:
-					response = ('302 Found', redirector(origi_URL_dic[num]))
+				if key in origi_URL_dic:
+					response = ('302 Found', router(origi_URL_dic[key]))
 				else:
-					response = send_response('404', "Resource Not Found!")
+					response = send_response('404', "<center>Resource Not Found!</center>")
 				
 			elif method == 'POST':
 				body = petition.split('\r\n\r\n', 1)[1]
-								 
-				if body == "URL=None" or body.find("URL=") == -1:
-					response = send_response('501', PRACTICE_NAME + "<h2><center>Server error in form!</center></h2>")
+				headers = petition.split('\r\n\r\n', 1)[0]
+				content_length = int(headers.split('Content-Length: ')[1].split('\r\n')[0])
+
+				if content_length < 5 or body.find("URL=") == -1:
+					response = send_response('200', "<h2><center>This server was unable to generate this URL.</center></h2>")
 				else:
 					_, url = body.split('=')
 					# To convert the replace(%3A and %2F ...) in the url. 
@@ -89,19 +83,20 @@ class practice1App(webapp.webApp):
 					
 					# I search if the URL isn't in the dictionary
 					if not url in simpli_URL_dic:
-						origi_URL_dic[len(origi_URL_dic)] = url
-						simpli_URL_dic[url] = len(origi_URL_dic) - 1
+						origi_URL_dic["/" + str(len(origi_URL_dic))] = url
+						simpli_URL_dic[url] = ("/" + str(len(origi_URL_dic) - 1))
 						
-					link = ("<h1><font color='Blue'><center>Choose one</center></font></h1>" +
-							"<h4>Your shortened URL: <a href='//localhost:1234/" + str(simpli_URL_dic[url]) + "'>http://localhost:1234/" + str(simpli_URL_dic[url]) + "</a>" +
+					links = ("<h2><font color='Blue'>Choose one:</font></h2>" +
+							"<h4>Your shortened URL: <a href='//" + str(machine) + ":" + str(port) + str(simpli_URL_dic[url]) +
+							"'>http://" + str(machine)+ ":" + str(port) + str(simpli_URL_dic[url]) + "</a>" +
 							"<br>Your original URL: <a href='" + str(origi_URL_dic[simpli_URL_dic[url]]) + "'>" + str(origi_URL_dic[simpli_URL_dic[url]]) + "</a></h4>")
-					response = send_response('200', PRACTICE_NAME + link)
+					response = send_response('200', links)
 			else:
-				response = send_response('404', PRACTICE_NAME + "Resource Not Found!")
+				response = send_response('501', "<center>Service not implemented on this server.</center>")
 		
 		"""Returns the HTTP code for the reply, and an HTML page."""
 		print(origi_URL_dic, simpli_URL_dic)
 		return response
 
 if __name__ == "__main__":
-    MyWebApp = practice1App("localhost", 1234)
+    MyWebApp = practice1App(machine, port)
